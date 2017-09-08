faf1705ad900730a5aa62c9af99f822e39c119d5
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-09-08 15:55:22 +0000

    added more colours

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ var chalk = require("chalk");
 
 var message = "Hello " + chalk.yellow("World");
 console.log(message);
+
+var playingWithColours = chalk.magenta("magenta") + chalk.bgBlue("highlight blue") + chalk.bold("i am bold") + chalk.underline("standing on a line") + chalk.green("GREENGREENGREEN")
+console.log(playingWithColours)
